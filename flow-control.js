bea8427e7d4cf9ecb50ee0116efb2a1bd4d1f0de
Marie Midford <marie.midford@gmail.com>
2019-11-09 04:21:47 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,6 +28,9 @@ return (age>=13 && age<=19) ? "You are a teenager" : "You are not a teenager"
 
 function switchAge(age) {
   switch (age) {
+    case "13":
+    return "You are a teenager";
+    break;
     case "14":
     return "You are a teeanger";
     break;
