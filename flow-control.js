8fb5d972091f0eb6873e5a7e0bffcebdf3c3309f
Marie Midford <marie.midford@gmail.com>
2019-11-09 03:38:17 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,7 +1,7 @@
 function basicTeenager(age) {
-if (age > 12) && (age < 20) {
-return "You are a teenager!"
-}
+  if (age > 12) && (age < 20) {
+    console.log("You are a teenager!")
+  }
 }
 
 function teenager(age) {
